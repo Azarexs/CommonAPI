2b57a1dd3c3c0a2357dd85bda1b29a2d28a9995a
Azarex <azarex.official@gmail.com>
2019-11-28 13:54:32 -0500

    Added ItemBuilder module

--- src/main/java/me/azarex/commonapi/CommonAPI.java
+++ src/main/java/me/azarex/commonapi/CommonAPI.java
@@ -1,10 +1,21 @@
 package me.azarex.commonapi;
 
+import me.azarex.commonapi.item.builder.ItemBuilder;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
 public class CommonAPI {
 
     private static CommonAPI instance;
 
     public static CommonAPI getInstance() {
+        final ItemStack sword = ItemBuilder.of(Material.DIAMOND_SWORD)
+                .setAmount(1)
+                .setDisplayName("The noob slayer!")
+                .setLore("This thing will kill all noobs", "every last one.")
+                .getItem();
+
+
         if (instance == null) {
             instance = new CommonAPI();
         }
--- src/main/java/me/azarex/commonapi/item/builder/ItemBuilder.java
+++ src/main/java/me/azarex/commonapi/item/builder/ItemBuilder.java
@@ -2,13 +2,51 @@ package me.azarex.commonapi.item.builder;
 
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
 
 public interface ItemBuilder {
 
     ItemStack getItem();
 
-    default ItemBuilder getType(Material type) {
+    default ItemBuilder setType(Material type) {
         getItem().setType(type);
         return this;
+    }
+
+    default ItemBuilder setAmount(int amount) {
+        getItem().setAmount(amount);
+        return this;
+    }
+
+    default ItemBuilder setDisplayName(String name) {
+        return applyToMeta(meta -> meta.setDisplayName(name));
+    }
+
+    default ItemBuilder setLore(String... lore) {
+        return applyToMeta(meta -> meta.setLore(Arrays.asList(lore)));
+    }
+
+    default ItemBuilder setLore(List<String> lore) {
+        return applyToMeta(meta -> meta.setLore(lore));
+    }
+
+    default <T extends ItemMeta> ItemBuilder applyToMeta(Consumer<T> metaAction) {
+        final ItemStack item = getItem();
+
+        if (item.getType() == Material.AIR) return this;
+        if (!item.hasItemMeta()) return this;
+
+        final ItemMeta meta = item.getItemMeta();
+        metaAction.accept((T)meta);
+        return this;
+    }
+
+    static ItemBuilder of(Material type) {
+        final ItemStack item = new ItemStack(type);
+        return () -> item;
     }
 }
